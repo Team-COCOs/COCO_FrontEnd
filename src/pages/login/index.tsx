f4e242139e34cd156f32a7387af72253f9777621
jeong0300 <dltnwjd8898@naver.com>
2025-04-30 16:24:37 +0900

    feat: main

--- src/pages/login/index.tsx
+++
@@ -1,7 +0,0 @@
-import Login from "@/features/LoginPage";
-
-const LoginPage = () => {
-  return <Login />;
-};
-
-export default LoginPage;
--- src/pages/main/index.tsx
+++
@@ -1,7 +0,0 @@
-import Main from "@/features/MainPage";
-
-const MainPage = () => {
-  return <Main />;
-};
-
-export default MainPage;
