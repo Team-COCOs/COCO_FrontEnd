44e323ef760ea1457e5e004503be256845019a94
jeong0300 <dltnwjd8898@naver.com>
2025-04-30 19:29:51 +0900

    feat: 메인 컬러

--- src/components/MainPage/ProfilePart/index.tsx
+++ src/components/MainPage/ProfilePart/index.tsx
@@ -1,11 +1,18 @@
-// import { MainPageStyled } from "./styled";
+import { ProfilePartStyle } from "./styled";
 import clsx from "clsx";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
+import Login from "@/components/MainPage/Login";
 
 const ProfilePart = () => {
-  return <div> 프로필 </div>;
+  return (
+    <ProfilePartStyle className={clsx("ProfilePart_wrap")}>
+      {/* 로고 컴포넌트 */}
+      <Login />
+      {/* 광고 컴포넌트 */}
+    </ProfilePartStyle>
+  );
 };
 
 export default ProfilePart;
