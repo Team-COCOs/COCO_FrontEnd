33359d31614a43ed19aeadd8d89f7f9205a0650a
ounjuu <koj1555@naver.com>
2025-05-07 21:27:33 +0900

    Feat: 로딩중 적용

--- src/features/MainPage/index.tsx
+++ src/features/MainPage/index.tsx
@@ -3,13 +3,30 @@ import ProfilePart from "@/components/MainPage/ProfilePart";
 import StorePart from "@/components/MainPage/StorePart";
 import Footer from "@/components/MainPage/Footer";
 import clsx from "clsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import Header from "@/components/MainPage/Header";
+import Loading from "@/components/Loading";
 
 const MainPage = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 로딩 상태 처리
+  useEffect(() => {
+    const fetchData = async () => {
+      // 예시로 1초 동안 기다린 후 로딩을 종료
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      setIsLoading(false); // 로딩 종료
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <MainPageStyled className={clsx("MainPage_wrap")}>
