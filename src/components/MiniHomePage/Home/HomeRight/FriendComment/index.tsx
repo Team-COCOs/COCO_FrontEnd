e3ee0ff3e9210769abdc4c3e9435a368e940d59a
ounjuu <koj1555@naver.com>
2025-05-08 20:52:31 +0900

    Feat: 일촌평 update

--- src/components/MiniHomePage/Home/HomeRight/FriendComment/index.tsx
+++ src/components/MiniHomePage/Home/HomeRight/FriendComment/index.tsx
@@ -3,9 +3,19 @@ import { useEffect, useState } from "react";
 import axiosInstance from "@/lib/axios";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
+import axios from "axios";
+interface FriendCommentData {
+  content: string;
+  createdAt: string;
+  authorName: string;
+  hostName: string;
+}
 
 const FriendComment = () => {
   const [comment, setComment] = useState("");
+  const [friendComment, setFriendComment] = useState<FriendCommentData | null>(
+    null
+  );
   const { user } = useAuth();
   const router = useRouter();
   const hostId = Number(router.query.id);
@@ -22,6 +32,8 @@ const FriendComment = () => {
 
       console.log("등록 성공:", response.data);
       setComment("");
+
+      fetchFriendComment();
     } catch (error) {
       console.error("등록 실패:", error);
     }
@@ -29,6 +41,25 @@ const FriendComment = () => {
     console.log("제출:", comment);
     setComment("");
   };
+
+  // 일촌평 가져오기
+  const fetchFriendComment = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/friend-comments/${hostId}`,
+        {
+          withCredentials: true,
+        }
+      );
+      setFriendComment(res.data.data);
+    } catch (error) {
+      console.error("일촌평 불러오기 실패", error);
+    }
+  };
+
+  useEffect(() => {
+    if (hostId) fetchFriendComment();
+  }, [hostId]);
 
   return (
     <FriendCommentStyled>
@@ -53,6 +84,14 @@ const FriendComment = () => {
           </div>
         </div>
       </div>
+      {friendComment && (
+        <div className="FriendComment_displayWrap">
+          <p>작성자: {friendComment.authorName}</p>
+          <p>별명: {friendComment.hostName}</p>
+          <p>작성일: {friendComment.createdAt}</p>
+          <p>내용: {friendComment.content}</p>
+        </div>
+      )}
     </FriendCommentStyled>
   );
 };
