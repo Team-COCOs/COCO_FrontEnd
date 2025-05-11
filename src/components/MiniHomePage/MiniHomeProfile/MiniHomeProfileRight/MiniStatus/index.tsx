360d1338983dd4e2c676a2a19a1387a3f54837e5
jeong0300 <dltnwjd8898@naver.com>
2025-05-12 01:42:20 +0900

    feat: 내 상태 관리 중

--- src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniStatus/index.tsx
+++ src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniStatus/index.tsx
@@ -1,19 +1,27 @@
 import { MiniStatusStyle } from "./styled";
 import Image from "next/image";
-import { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+import { useRef, useState } from "react";
 
 const MiniStatus = () => {
   const [profileImage, setProfileImage] = useState<string>(
     "/avatarImg/defaultProfile.png"
   );
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { user } = useAuth();
 
+  // 이미지 url로 변환 후 업데이트
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      // 파일을 URL로 변환 후 반영
       const fileUrl = URL.createObjectURL(file);
       setProfileImage(fileUrl);
     }
+  };
+
+  // 해당 돔 요소에 접근 -> 파일 열기
+  const handleUploadClick = () => {
+    fileInputRef.current?.click();
   };
 
   return (
@@ -25,19 +33,59 @@ const MiniStatus = () => {
           <div className="MiniStatus_img">
             <Image src={profileImage} alt="profile" fill />
           </div>
-          <div className="MiniStatus_upload Gulim">
-            <label htmlFor="profile-upload">프로필 선택</label>
+
+          <div
+            className="MiniStatus_upload pixelFont"
+            onClick={handleUploadClick}
+          >
+            프로필 선택
             <input
               type="file"
               id="profile-upload"
               className="MiniStatus_input"
+              ref={fileInputRef}
               onChange={handleFileChange}
+              accept="image/*"
             />
           </div>
         </div>
+
+        <div className="MiniStatus_right">
+          <div className="MiniStatus_state">
+            <span className="pixelFont">Today is...</span>
+            <select className="MiniStatus_select pixelFont">
+              <option value="happy">😊 행복</option>
+              <option value="joy">🎵 즐거움</option>
+              <option value="busy">💼 바쁨</option>
+              <option value="sad">🌧️ 슬픔</option>
+              <option value="angry">💢 화남</option>
+            </select>
+          </div>
+          <div className="MiniStatus_introduce">
+            <span className="pixelFont">소개글</span>
+            <textarea
+              className="Gulim"
+              placeholder="50자 이내로 내 소개를 적어보세요~"
+            />
+          </div>
+          ㄴ
+        </div>
       </div>
 
-      <div className="MiniStatus_bottom"></div>
+      <div className="MiniStatus_bottom">
+        <div className="MiniStatus_name">
+          <span className="pixelFont">미니홈피 이름</span>
+          <input
+            className="Gulim"
+            type="text"
+            placeholder={`${user?.name}님의 미니홈피`}
+          />
+        </div>
+      </div>
+
+      <div className="MiniStatus_footer">
+        <button className="MiniStatus_btn pixelFont">저장</button>
+      </div>
     </MiniStatusStyle>
   );
 };
--- src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniStatus/styled.ts
+++ src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniStatus/styled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const MiniStatusStyle = styled.div`
   &.MiniStatus_wrap {
+    height: 100%;
     display: flex;
     flex-direction: column;
     padding-top: 10px;
@@ -18,12 +19,14 @@ export const MiniStatusStyle = styled.div`
     .MiniStatus_top {
       display: flex;
       gap: 10px;
+      padding: 10px 0px;
 
-      .MiniStatus_left {
+      .MiniStatus_left,
+      .MiniStatus_right {
         display: flex;
         flex-direction: column;
         align-items: center;
-        gap: 10px;
+        gap: 15px;
         width: 50%;
 
         .MiniStatus_img {
@@ -35,10 +38,16 @@ export const MiniStatusStyle = styled.div`
         .MiniStatus_upload {
           width: 90px;
           text-align: center;
-          font-size: 13px;
-          border: 1px solid rgb(186, 186, 186);
+          font-size: 12px;
+          background: linear-gradient(to bottom, #ffffff, #dddddd);
+          border: 1px solid #000000;
+          border-radius: 3px;
           padding: 4px 8px;
           cursor: pointer;
+
+          &:active {
+            background: linear-gradient(to top, #ffffff, #dddddd);
+          }
 
           label {
             cursor: pointer;
@@ -47,6 +56,111 @@ export const MiniStatusStyle = styled.div`
           .MiniStatus_input {
             display: none;
           }
+        }
+      }
+
+      .MiniStatus_right {
+        padding: 10px;
+
+        .MiniStatus_state {
+          width: 100%;
+          display: flex;
+          align-items: center;
+          gap: 25px;
+          border: 1px solid rgb(201, 201, 201);
+          padding: 10px;
+          margin-top: 10px;
+
+          .MiniStatus_select {
+            width: 150px;
+            padding: 5px;
+
+            &:active,
+            &:focus {
+              outline: none;
+            }
+          }
+        }
+
+        .MiniStatus_introduce {
+          width: 100%;
+          margin-top: -5px;
+
+          span {
+            font-size: 14px;
+          }
+
+          textarea {
+            margin-top: 5px;
+            width: 100%;
+            height: 138px;
+            border: 1px solid rgb(201, 201, 201);
+            padding: 10px;
+
+            &:active,
+            &:focus {
+              outline: none;
+            }
+          }
+        }
+      }
+
+      @media (max-width: 768px) {
+        & {
+          flex-direction: column;
+
+          .MiniStatus_left,
+          .MiniStatus_right {
+            width: 100%;
+          }
+        }
+      }
+    }
+
+    .MiniStatus_bottom {
+      width: 100%;
+      padding: 10px;
+
+      .MiniStatus_name {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        span {
+          font-size: 14px;
+        }
+
+        input {
+          width: 100%;
+          height: 30px;
+          padding: 10px;
+          border: 1px solid rgb(201, 201, 201);
+
+          &:active,
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+    }
+
+    .MiniStatus_footer {
+      display: flex;
+      justify-content: flex-end;
+      margin-top: auto;
+      width: 100%;
+      padding: 10px;
+
+      .MiniStatus_btn {
+        padding: 3px;
+        width: 40px;
+        font-size: 13px;
+        background: linear-gradient(to bottom, #ffffff, #dddddd);
+        border: 1px solid #000000;
+        border-radius: 3px;
+
+        &:active {
+          background: linear-gradient(to top, #ffffff, #dddddd);
         }
       }
     }
