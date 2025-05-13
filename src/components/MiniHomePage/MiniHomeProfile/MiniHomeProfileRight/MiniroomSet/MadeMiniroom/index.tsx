817242a855ae567c96cc65e9d35d1f67611e81d4
ounjuu <koj1555@naver.com>
2025-05-13 12:10:18 +0900

    미니룸 이름 반영2

--- src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniroomSet/MadeMiniroom/index.tsx
+++ src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniroomSet/MadeMiniroom/index.tsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { MadeMiniroomStyled } from "./styled";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 interface MadeMiniroomProps {
   setfixMiniroom: (value: boolean) => void;
 }
 
 const MadeMiniroom: React.FC<MadeMiniroomProps> = ({ setfixMiniroom }) => {
+  const { query } = useRouter();
+  const { id } = query;
+
+  // 미니룸 이름 관리
+  const [miniroomName, setMiniroomName] = useState("");
+
+  // 미니룸 이름 불러오기
+  useEffect(() => {
+    const fetchMiniroomName = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/minirooms/${id}/title`
+        );
+        setMiniroomName(response.data.title);
+      } catch (e: any) {
+        console.log(e, "미니룸 이름 e");
+      }
+    };
+    fetchMiniroomName();
+  }, [id]);
+
   return (
     <MadeMiniroomStyled>
       <div className="MinimiSet_wrap">
         <div className="MadeMiniroom_titleWrap">
           <div className="MadeMiniroom_wrap_title Gulim">
-            미니룸
+            {miniroomName === null || miniroomName === undefined
+              ? "미니룸"
+              : miniroomName}
             <span onClick={() => setfixMiniroom(true)}>✏️</span>
           </div>
           <div className="MadeMiniroom_imgWrap">
