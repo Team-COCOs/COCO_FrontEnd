97dfae7ec6ec13a14e8fa81e08066a6958636788
jeong0300 <dltnwjd8898@naver.com>
2025-05-17 23:08:16 +0900

    feat: 내 계정 css

--- src/components/MiniHomePage/Setting/SettingRight/SettingMyInfo/index.tsx
+++ src/components/MiniHomePage/Setting/SettingRight/SettingMyInfo/index.tsx
@@ -1,13 +1,74 @@
+import { useRouter } from "next/router";
 import { SettingMyInfoStyle } from "./styled";
 
 const SettingMyInfo = () => {
+  const router = useRouter();
+
   return (
     <SettingMyInfoStyle className="SettingMyInfo_wrap">
       <div className="SettingMyInfo_header">
         <div className="SettingMyInfo_title Gulim">내 정보 수정하기</div>
       </div>
 
-      <div className="SettingMyInfo_body"></div>
+      <div className="SettingMyInfo_body">
+        <div
+          className="SettingMyInfo_myBase Gulim"
+          onClick={() => router.push("https://cyworld.com/")}
+        >
+          미니홈피 외 싸이월드 서비스의 사생활보호 설정은 마이베이스에서 <br />
+          안내 받으시거나, 설정하실 수 있습니다.
+          <div className="SettingMyInfo_myBaseText">
+            마이베이스 가기 <span className="SettingMyInfo_myBaseIcon">▶</span>
+          </div>
+        </div>
+
+        <span className="pixelFont SettingMyInfo_subTitle">
+          <div className="SettingMyInfo_iconText">
+            <span className="SettingMyInfo_icon">🟧</span>비밀번호 변경
+          </div>
+          <span className="SettingMyInfo_subText">
+            * 10자 이상, 숫자, 특수문자 포함
+          </span>
+        </span>
+
+        <div className="SettingMyInfo_password">
+          <div className="SettingMyInfo_textBox">
+            <div className="SettingMyInfo_text Gulim">비밀번호</div>
+            <p className="SettingMyInfo_error">
+              10자 이상, 숫자, 특수문자를 포함하여주세요.
+            </p>
+            <input type="password"></input>
+          </div>
+
+          <div className="SettingMyInfo_underLine"></div>
+
+          <div className="SettingMyInfo_textBox">
+            <div className="SettingMyInfo_text Gulim">비밀번호 확인</div>
+            <input type="password"></input>
+            <p className="SettingMyInfo_error"></p>
+          </div>
+        </div>
+
+        <div className="SettingMyInfo_btns">
+          <button>확인</button>
+        </div>
+
+        <span className="pixelFont SettingMyInfo_subTitle">
+          <div className="SettingMyInfo_iconText">
+            <span className="SettingMyInfo_icon">🟧</span>전화번호 변경
+          </div>
+        </span>
+
+        <div className="SettingMyInfo_password">
+          <div className="SettingMyInfo_textBox">
+            <div className="SettingMyInfo_text Gulim">전화번호</div>
+            <input type="text"></input>
+          </div>
+        </div>
+        <div className="SettingMyInfo_btns">
+          <button>확인</button>
+        </div>
+      </div>
     </SettingMyInfoStyle>
   );
 };
--- src/components/MiniHomePage/Setting/SettingRight/SettingMyInfo/styled.ts
+++ src/components/MiniHomePage/Setting/SettingRight/SettingMyInfo/styled.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const SettingMyInfoStyle = styled.div`
   &.SettingMyInfo_wrap {
     width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 
     .SettingMyInfo_header {
       color: #4b4b4b;
@@ -16,6 +19,170 @@ export const SettingMyInfoStyle = styled.div`
         display: flex;
         flex-direction: column;
         padding-top: 10px;
+      }
+    }
+
+    .SettingMyInfo_body {
+      display: flex;
+      flex-direction: column;
+      gap: 15px;
+
+      .SettingMyInfo_myBase {
+        padding: 10px 15px;
+        font-size: 13px;
+        border-top: 1.5px solid rgb(177, 177, 177);
+        border-bottom: 1.5px solid rgb(194, 194, 194);
+        background-color: rgb(249, 249, 249);
+
+        @media (max-width: 1003px) {
+          font-size: 11px;
+        }
+
+        @media (max-width: 890px) {
+          font-size: 10px;
+        }
+
+        .SettingMyInfo_myBaseText {
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+          gap: 5px;
+          color: rgb(62, 209, 235);
+          font-weight: bold;
+          cursor: pointer;
+
+          .SettingMyInfo_myBaseIcon {
+            color: ${({ theme }) => theme.colors.mainColor};
+            font-size: 6px;
+          }
+        }
+      }
+
+      .SettingMyInfo_subTitle {
+        display: flex;
+        gap: 5px;
+        align-items: center;
+        font-size: 13.5px;
+
+        @media (max-width: 520px) {
+          flex-direction: column;
+          align-items: start;
+          justify-content: center;
+          font-size: 13px;
+        }
+
+        .SettingMyInfo_iconText {
+          display: flex;
+          align-items: center;
+          gap: 5px;
+        }
+
+        .SettingMyInfo_icon {
+          font-size: 5px;
+        }
+
+        .SettingMyInfo_subText {
+          font-size: 11px;
+          color: rgb(142, 142, 142);
+          margin-left: 5px;
+
+          @media (max-width: 520px) {
+            font-size: 10px;
+          }
+        }
+      }
+
+      .SettingMyInfo_password {
+        display: flex;
+        flex-direction: column;
+        border-top: 1.5px solid rgb(177, 177, 177);
+        border-bottom: 1.5px solid rgb(194, 194, 194);
+        background-color: rgb(249, 249, 249);
+
+        .SettingMyInfo_textBox {
+          display: flex;
+          align-items: center;
+          justify-content: space-around;
+          padding: 15px;
+          width: 100%;
+          position: relative;
+
+          @media (max-width: 588px) {
+            flex-direction: column;
+          }
+
+          .SettingMyInfo_text {
+            width: 20%;
+            display: flex;
+            font-size: 13px;
+            justify-content: center;
+
+            @media (max-width: 1003px) {
+              width: 25%;
+              font-size: 11px;
+            }
+
+            @media (max-width: 588px) {
+              width: 100%;
+            }
+          }
+
+          input {
+            width: 75%;
+            height: 25px;
+            padding: 5px;
+            margin: 5px 0;
+
+            &:focus,
+            &:active {
+              outline: none;
+            }
+
+            @media (max-width: 588px) {
+              width: 90%;
+            }
+          }
+
+          .SettingMyInfo_error {
+            width: 100%;
+            position: absolute;
+            bottom: 3px;
+            text-align: center;
+            color: rgb(255, 105, 105);
+            font-size: 12px;
+
+            @media (max-width: 430px) {
+              font-size: 10px;
+            }
+
+            @media (max-width: 370px) {
+              font-size: 9px;
+            }
+          }
+        }
+
+        .SettingMyInfo_underLine {
+          width: 100%;
+          height: 1px;
+          border-bottom: 1px dotted rgb(167, 167, 167);
+        }
+      }
+
+      .SettingMyInfo_btns {
+        padding: 0 10px;
+        display: flex;
+        justify-content: flex-end;
+
+        button {
+          padding: 4px 5px;
+          background-color: rgb(248, 248, 248);
+          color: #4b4b4b;
+          font-size: 11px;
+          border: 1px solid #4b4b4b;
+          border-radius: 2px;
+          cursor: pointer;
+          font-weight: bold;
+        }
       }
     }
   }
