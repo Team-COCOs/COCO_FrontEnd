1c899bf0362d8324d8dc39c6a2e1ac9125f17487
ounjuu <koj1555@naver.com>
2025-05-19 10:24:36 +0900

    Feat: 댓글 답글 css

--- src/components/MiniHomePage/Diary/DiaryRight/CommentDiary/DiaryComments/index.tsx
+++ src/components/MiniHomePage/Diary/DiaryRight/CommentDiary/DiaryComments/index.tsx
@@ -21,14 +21,39 @@ const DiaryComments = () => {
     <DiaryCommentsStyle>
       <div className="DiaryComments_wrap Gulim">
         <div className="DiaryComments_mapwrap Gulim">
-          <span className="DiaryComments_author">김하나 : </span>
-          <span className="DiaryComments_content">내용</span>
-          <span className="DiaryComments_date">(2025.01.01)</span>
-          <img
-            src="/arrowIcon.png"
-            alt="arrow-icon"
-            className="DiaryComments_arrowicon"
-          />
+          <div className="DiaryComments_parentComment">
+            <span className="DiaryComments_author">김하나 : </span>
+            <span className="DiaryComments_content">내용</span>
+            <span className="DiaryComments_date">(2025.01.01)</span>
+            <img
+              src="/arrowIcon.png"
+              alt="arrow-icon"
+              className="DiaryComments_arrowicon"
+            />
+            <span className="DiaryComments_comment_deletebtn">☒</span>
+          </div>
+
+          <div className="DiaryComments_childrenComment">
+            <div className="DiaryComments_inputWrap">
+              <label
+                className="DiaryComments_commentLabel"
+                htmlFor="DiaryComments_comment-input"
+              >
+                답글
+              </label>
+              <input
+                className="DiaryComments_commentInput"
+                id="DiaryComments_comment-input"
+                type="text"
+              ></input>
+              <button className="DiaryComments_commentBtn">확인</button>
+            </div>
+            <span className="DiaryComments_childarrow">⤷ </span>
+            <span className="DiaryComments_author">김하나 : </span>
+            <span className="DiaryComments_content">내용</span>
+            <span className="DiaryComments_date">(2025.01.01)</span>
+            <span className="DiaryComments_comment_deletebtn">☒</span>
+          </div>
         </div>
       </div>
     </DiaryCommentsStyle>
--- src/components/MiniHomePage/Diary/DiaryRight/CommentDiary/DiaryComments/styled.ts
+++ src/components/MiniHomePage/Diary/DiaryRight/CommentDiary/DiaryComments/styled.ts
@@ -6,12 +6,6 @@ export const DiaryCommentsStyle = styled.div`
   }
   .DiaryComments_mapwrap {
     font-size: 11px;
-    img {
-      width: 10px;
-      margin-left: 5px;
-      padding-top: 5px;
-      cursor: pointer;
-    }
   }
   .DiaryComments_author {
     color: navy;
@@ -19,11 +13,16 @@ export const DiaryCommentsStyle = styled.div`
     letter-spacing: 1px;
     font-weight: bold;
   }
+
   .DiaryComments_content {
     color: #4b4b4b;
     font-size: 11.5px;
     letter-spacing: 1px;
   }
+
+  /* .DiaryComments_childarrow {
+    padding-top: 5px;
+  } */
 
   .DiaryComments_date {
     color: #bbb;
@@ -32,7 +31,51 @@ export const DiaryCommentsStyle = styled.div`
     font-weight: bold;
     padding-left: 5px;
   }
+  .DiaryComments_parentComment {
+    padding: 3px;
+    img {
+      width: 10px;
+      margin-left: 5px;
+      padding-top: 10px;
+      cursor: pointer;
+    }
+  }
+  .DiaryComments_childrenComment {
+    padding: 3px 0px 3px 55px;
+  }
+  .DiaryComments_childarrow {
+    color: gray;
+  }
+  .DiaryComments_comment_deletebtn {
+    color: #aaa;
+    padding-left: 5px;
+    padding-top: 3px;
+    cursor: pointer;
+  }
 
+  .DiaryComments_inputWrap {
+    margin: 10px 0px;
+  }
+  .DiaryComments_commentLabel {
+    font-size: 11px;
+    font-weight: bold;
+    width: 5%;
+  }
+  .DiaryComments_commentInput {
+    padding: 0px 5px;
+    margin: 0px 5px;
+    width: 65%;
+  }
+  /* 버튼 */
+  .DiaryComments_commentBtn {
+    padding: 2px 5px;
+    background-color: rgb(248, 248, 248);
+    color: #4b4b4b;
+    font-size: 10px;
+    border: 1px solid #4b4b4b;
+    border-radius: 2px;
+    cursor: pointer;
+  }
   /* 반응형 */
   @media (max-width: 1024px) {
   }
