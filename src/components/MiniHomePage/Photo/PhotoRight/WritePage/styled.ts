58aa6021d73313defddbd0814b89a1dbd12ac563
jeong0300 <dltnwjd8898@naver.com>
2025-05-19 14:23:50 +0900

    feat: write css

--- src/components/MiniHomePage/Photo/PhotoRight/WritePage/styled.ts
+++ src/components/MiniHomePage/Photo/PhotoRight/WritePage/styled.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const WritePageStyle = styled.div`
   &.WritePage_wrap {
     width: 100%;
+    height: 98%;
+    overflow-y: auto;
     padding: 20px 40px;
     display: flex;
     flex-direction: column;
@@ -138,6 +140,56 @@ export const WritePageStyle = styled.div`
           background: linear-gradient(to top, #ffffff, #dddddd);
         }
       }
+    }
+
+    /* 스크롤바 커스텀 */
+    &::-webkit-scrollbar {
+      width: 16px;
+      background-color: #f0f0f0;
+    }
+
+    &::-webkit-scrollbar-track {
+      background-color: rgb(237, 237, 237);
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: rgb(215, 215, 215);
+    }
+    &::-webkit-scrollbar-thumb:hover {
+      background-color: rgb(206, 206, 206);
+    }
+
+    &::-webkit-scrollbar-button {
+      background-color: #e0e0e0;
+      height: 16px;
+      display: block;
+      background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><polygon points='5,2 2,6 8,6' fill='%23666'/></svg>");
+      background-repeat: no-repeat;
+      background-position: center;
+    }
+
+    &::-webkit-scrollbar-button:single-button:vertical:decrement {
+      height: 16px;
+      background-color: #e0e0e0;
+      background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><polygon points='5,2 2,6 8,6' fill='%23ffffff'/></svg>");
+      background-repeat: no-repeat;
+      background-position: center;
+      border-bottom: 1px solid rgb(255, 255, 255);
+    }
+
+    /* 스크롤바 버튼 아래쪽 (▼) */
+    &::-webkit-scrollbar-button:single-button:vertical:increment {
+      height: 16px;
+      background-color: #e0e0e0;
+      background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><polygon points='5,8 2,4 8,4' fill='%23ffffff'/></svg>");
+      background-repeat: no-repeat;
+      background-position: center;
+      border-top: 1px solid rgb(255, 255, 255);
+    }
+
+    /* 나머지 버튼 제거 */
+    &::-webkit-scrollbar-button:vertical:start:increment,
+    &::-webkit-scrollbar-button:vertical:end:decrement {
+      display: none;
     }
   }
 `;
