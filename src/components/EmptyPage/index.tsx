f72cc11b8d912d8782370c68da3f6d30e42031a1
jeong0300 <dltnwjd8898@naver.com>
2025-05-22 16:07:15 +0900

    fix: 스켈레톤 제거

--- src/components/EmptyPage/index.tsx
+++ src/components/EmptyPage/index.tsx
@@ -1,32 +1,16 @@
 import { useState } from "react";
 import { EmptyPageStyle } from "./styeld";
 import Image from "next/image";
-import Skeleton from "@mui/material/Skeleton";
 
 interface EmptyProps {
   type?: string;
 }
 
 const EmptyPage = ({ type }: EmptyProps) => {
-  const [loaded, setLoaded] = useState(false);
-
   return (
     <EmptyPageStyle className="EmptyPage_wrap">
       <div className={`Empty_img ${type || ""}`}>
-        {!loaded && (
-          <Skeleton
-            variant="rectangular"
-            width="100%"
-            height="100%"
-            sx={{ position: "absolute", top: 0, left: 0, zIndex: 1 }}
-          />
-        )}
-        <Image
-          src="/dotori/emptyImg.png"
-          alt="empty image"
-          fill
-          onLoad={() => setLoaded(true)}
-        />
+        <Image src="/dotori/emptyImg.png" alt="empty image" fill />
       </div>
     </EmptyPageStyle>
   );
