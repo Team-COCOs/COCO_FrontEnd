8466e6afeff86758092185215e95862a0443480e
jeong0300 <dltnwjd8898@naver.com>
2025-05-22 15:57:12 +0900

    feat: 스켈레톤, 모달 수정

--- src/components/EmptyPage/index.tsx
+++ src/components/EmptyPage/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import { EmptyPageStyle } from "./styeld";
 import Image from "next/image";
+import Skeleton from "@mui/material/Skeleton";
 
 interface EmptyProps {
   type?: string;
 }
 
 const EmptyPage = ({ type }: EmptyProps) => {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <EmptyPageStyle className="EmptyPage_wrap">
       <div className={`Empty_img ${type || ""}`}>
-        <Image src="/dotori/emptyImg.png" alt="empty image" fill />
+        {!loaded && (
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height="100%"
+            sx={{ position: "absolute", top: 0, left: 0, zIndex: 1 }}
+          />
+        )}
+        <Image
+          src="/dotori/emptyImg.png"
+          alt="empty image"
+          fill
+          onLoad={() => setLoaded(true)}
+        />
       </div>
     </EmptyPageStyle>
   );
