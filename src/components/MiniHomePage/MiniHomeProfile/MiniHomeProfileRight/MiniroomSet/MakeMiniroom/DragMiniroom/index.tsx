e9882073e5b929580216df40b03172abece9b23d
ounjuu <koj1555@naver.com>
2025-05-25 11:49:09 +0900

    Feat: 콘솔 제거

--- src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniroomSet/MakeMiniroom/DragMiniroom/index.tsx
+++ src/components/MiniHomePage/MiniHomeProfile/MiniHomeProfileRight/MiniroomSet/MakeMiniroom/DragMiniroom/index.tsx
@@ -120,29 +120,7 @@ const DragMiniroom: React.FC<DragMiniroomProps> = ({
         });
       }
     });
-    // console.log(initialItems, "initialItems");
-    // // ✅ 말풍선이 누락된 경우 강제로 추가
-    // const hasSpeechBubble = updatedItems.some(
-    //   (item) => item.type === "speechBubble"
-    // );
 
-    // if (!hasSpeechBubble) {
-    //   const speechBubble = initialItems.find(
-    //     (item) => item.type === "speechBubble"
-    //   );
-
-    //   if (speechBubble) {
-    //     layoutData.push({
-    //       id: speechBubble.id,
-    //       type: "speechBubble",
-    //       x: speechBubble.left ?? 0,
-    //       y: speechBubble.top ?? 0,
-    //       text: speechBubble.text ?? "",
-    //     });
-    //   }
-    // }
-
-    // console.log(initialItems, "initialItems2");
     onDragComplete(
       layoutData.map((item) => {
         const base = {
